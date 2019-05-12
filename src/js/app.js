cd9d2e55adaea164702c0ea5a31be14cf8d4decb
Rumbiiha swaibu <rumbiihas@gmail.com>
2019-05-12 21:01:26 +0300

    Setup mini layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
+// humberger style  
+  $(".js-hamburger").click(function(){
+    $("body").toggleClass("mini");
+    $(".hamburger-toggle").toggleClass("is-opened");
+  });
